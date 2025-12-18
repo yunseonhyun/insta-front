63557ae0246e39008b98f555922a8fd72d3897fb
yunseonhyun <70725625+yunseonhyun@users.noreply.github.com>
2025-12-18 15:10:06 +0900

    vercel.json

--- src/service/apiService.js
+++ src/service/apiService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 
-export const API_BASE_URL = 'http://3.38.199.131:9000/api';
+export const API_BASE_URL = 'https://3.38.199.131:9000/api';
 
 const api = axios.create({
     baseURL: API_BASE_URL,
